c369e1b154b0b679c232ce625e87647b220b84c7
Khalifa S <ibrahim.idris.developer@gmail.com>
2021-10-10 13:49:40 +0100

    Setup mysql driver

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,29 @@
 package main
 
 import (
-  "gorm.io/gorm"
-  "gorm.io/driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+type User struct {
+	gorm.Model
+	FirstName string
+	LastName  string
+	Email     string
+	Password  string
+	Colour    string
+}
+
+func main() {
+	dsn := "user:pass@tcp(url:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect database")
+	}
+
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		panic("Failed to migrate DB")
+	}
+}
