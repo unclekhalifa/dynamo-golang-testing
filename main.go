276a7532b06d9322b11843ef7c3243437d147b76
Ibrahim Idris <ibrahim.idris.developer@gmail.com>
2021-10-23 18:53:50 +0100

    Use .env to store db info

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
+	"os"
 )
 
 type User struct {
@@ -15,9 +19,21 @@ type User struct {
 }
 
 func main() {
-	dsn := "user:pass@tcp(url:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Some error occured: %s", err)
+	}
+
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PWD"),
+		os.Getenv("DB_ENDPOINT"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"))
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect database")
 	}
