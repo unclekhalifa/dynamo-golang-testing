3fe0ceed7cf53f7fc10cbba10ebb7ffe55d42c9a
Ibrahim Idris <ibrahim.idris.developer@gmail.com>
2021-10-29 07:44:31 +0100

    Update test create data

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 		panic("Failed to migrate DB")
 	}
 
-	db.Create(&User{FirstName: "John", LastName: "Doe", Email: "john@doe.com", Password: "password", Colour: "#efefef"})
+	db.Create(&User{FirstName: "John", LastName: "Doe", Email: "john@mail.com", Password: "password", Colour: "#efefef"})
 
 	var user User
 	db.First(&user, "FirstName = ?", "John")
